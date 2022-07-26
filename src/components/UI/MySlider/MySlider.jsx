6504ca5863a36013a8c7e41fe16c7b983903ebdb
Tobolich Alexander <frezar2007@gmail.com>
2022-07-26 15:26:30 +0300

    add no img

--- src/components/UI/MySlider/MySlider.jsx
+++ src/components/UI/MySlider/MySlider.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import CarInfoStore from "../../../store/CarInfoStore";
+import noPhoto from '../../../img/no-photo.png'
 
 const MySlider = ({data = [1, 1, 1, 1, 1], infiniteLoop, emulateTouch}) => {
-    console.log(CarInfoStore.getCarInfo()['image'].split())
+    const [imgError, setImgError] = useState(false)
     return (
         <div>
             <Carousel
@@ -16,15 +17,27 @@ const MySlider = ({data = [1, 1, 1, 1, 1], infiniteLoop, emulateTouch}) => {
                 emulateTouch={emulateTouch}
                 infiniteLoop={infiniteLoop}>
                 {
+                    CarInfoStore.getCarInfo()['image']
+                        ?
                     CarInfoStore.getCarInfo()['image'].split().map((e,index) =>
                         <div key={index}>
                             <img
                                 style={{transform:'scale(0.7)'}}
                                 // src={`https://stock.aps.by${e}`}
-                                src={e}
-                                alt="car"/>
+                                // src={e}
+                                src={imgError ? noPhoto : '/img/' + e} alt="#"
+                                onError={(e) => {
+                                    if (e.type === 'error') {
+                                        setImgError(true)
+                                    }
+                                }}
+                            />
                         </div>
                     )
+                        : <img
+                            style={{transform:'scale(1)'}}
+                            src={noPhoto} alt="#"
+                        />
                 }
 
             </Carousel>
