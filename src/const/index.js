762e1c6db88217ce7f62206de6f03a0aaa0b596e
frezar <frezar@mail.ru>
2022-11-24 10:07:55 +0300

    add preselect all filters

--- src/const/index.js
+++ src/const/index.js
@@ -57,13 +57,32 @@ export const CheckUrlAndClickInput = () =>{
     const brand = url.searchParams.get("brand");
     const model =  url.searchParams.get("model");
     const promo =  url.searchParams.get("promo");
+    const body =  url.searchParams.get("body");
+    const fuelType =  url.searchParams.get("fuel");
+    const transmissionType =  url.searchParams.get("transmission");
+    const driveTypeId =  url.searchParams.get("drive");
+    const color =  url.searchParams.get("color");
+    const location =  url.searchParams.get("location");
 
     let blockInput = document.querySelector(`input[name="${brand}"]`)
     let modelInput = document.querySelector(`input[name="${model}"]`)
     let promoInput = document.querySelector(`input[name="${promo}"]`)
+    let bodyInput = document.querySelector(`input[name="${body}"]`)
+    let fuelTypeInput = document.querySelector(`input[name="${fuelType}"]`)
+    let transmissionTypeInput = document.querySelector(`input[name="${transmissionType}"]`)
+    let driveTypeInput = document.querySelector(`input[name="${driveTypeId}"]`)
+    let colorInput = document.querySelector(`input[name="${color}"]`)
+    let locationInput = document.querySelector(`input[name="${location}"]`)
+
     ClickInput(blockInput)
     ClickInput(modelInput)
     ClickInput(promoInput)
+    ClickInput(bodyInput)
+    ClickInput(fuelTypeInput)
+    ClickInput(transmissionTypeInput)
+    ClickInput(driveTypeInput)
+    ClickInput(colorInput)
+    ClickInput(locationInput)
     function ClickInput(block){
         if (block) {
             if (!block.checked) {
