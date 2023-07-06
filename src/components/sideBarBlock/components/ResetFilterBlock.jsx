cee85d41a9d52a91bdcb013c2b1b106b303bb0fe
Tobolich Alexander <frezar@mail.ru>
2023-07-06 12:27:49 +0300

    t

--- src/components/sideBarBlock/components/ResetFilterBlock.jsx
+++ src/components/sideBarBlock/components/ResetFilterBlock.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import s from './ResetFilterBlock.module.scss'
 import MyButton from "../../UI/MyButton";
+import CarsStore from "../../../store/carsStore";
+import ChangeFormStore from "../../../store/changeFormStore";
 
 const ResetFilterBlock = () => {
+    const reset = ()=>{
+        document.querySelectorAll('input[type="checkbox"]:checked').forEach(el=>{
+            el.click()
+        })
+    }
     return (
         <div className={s.resetBlock}>
-            <MyButton style={{height:50,fontSize:16}} >Очистить фильтр</MyButton>
+            <MyButton onClick={reset} style={{height:50,fontSize:16}} >Очистить фильтр</MyButton>
         </div>
     );
 };
